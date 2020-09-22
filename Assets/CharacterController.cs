12a723b62bf22f6ad6003ccc21d3bbca562fe8e7
BenJLubrano <benjaminlubrano@gmail.com>
2020-09-22 17:35:09 -0500

    Created CharacterController.cs
    Created the script that will hold all character movement logic, and added some very basic code. Some more progress to the rest of the game must be made, namely creating a basic test scene, before any more work can be done on this branch.

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script will handle all of the player's movement
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 10;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalMove, verticalMove);
+
+        rb.AddForce(movement * speed);
+    }
+}
