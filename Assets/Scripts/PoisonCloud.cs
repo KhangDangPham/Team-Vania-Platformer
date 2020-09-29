ba39f575293512bdd86be0c4ad9a83d36970d98c
BenJLubrano <benjaminlubrano@gmail.com>
2020-09-29 17:20:47 -0500

    Added Spike Trap, Poison Cloud Trap, and Level Ender
    The spike trap will instantly kill the player if he touches it, the poison cloud deals damage once every second or so, and the level ender brings the player to the next level once they hit it.

---
+++ Assets/Scripts/PoisonCloud.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonCloud : MonoBehaviour
+{
+    public float lifeTime = 10f;
+    public BoxCollider2D cloudZone;
+
+    private void Start()
+    {
+        cloudZone = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered");
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<CharacterController>().TakeDamage(25);
+        }
+    }
+
+    void Update()
+    {
+        if((int)lifeTime % 2 == 0)
+        {
+            Debug.Log("Disabling");
+            cloudZone.enabled = false;
+        }
+        else
+        {
+            Debug.Log("Enabling");
+            cloudZone.enabled = true;
+        }
+        lifeTime -= Time.deltaTime;
+        if(lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
