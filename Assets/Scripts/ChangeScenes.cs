6fbd13b2289de3764f454171440cf7de7f3441aa
Jerrin Wiley <jerrintwiley@gmail.com>
2020-11-10 14:27:36 -0600

    Revert "Merge branch 'master' into Jerrin-Issue35"
    This reverts commit 1aeaf73aa8c67cd4e9b4ccdd589e6ce43d91f6a7, reversing
    changes made to acd6dee770283896c303c90e70ddab1c426d08d9.

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -1,37 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class ChangeScenes : MonoBehaviour
 {
-    public static ChangeScenes instance = null;
-
-    private void Awake()
-    {
-        //Start at Intro Scene
-        instance = this;
-        if(SceneManager.GetActiveScene().name == "Team Intro Scene")
-        {
-            Invoke("loadStart", 5f);
-        }
-    }
-   
-   //Scene Changer
    public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
-    }
-    //Load Start - Might be used later
-    void loadStart()
-    {
-        ChangeScene("Start Menu");
-    }
-    //Exit Game
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 }
