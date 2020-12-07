971c1443ef7d421583344d279972867df9dfbb14
BenJLubrano <benjaminlubrano@gmail.com>
2020-12-06 20:37:47 -0600

    Changed the Cave Scene and Fixed player sticking to platform (Undo this commit if conflicts are found)

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -15,6 +15,16 @@ public class Platform : MonoBehaviour
         nextPos = startPos.position;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.gameObject.transform.SetParent(null);
+    }
+
     // Update is called once per frame
     void Update()
     {
