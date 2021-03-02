bb0b3a10783aefa8bd3d7b18e1be70761daa904a
trithai1 <trithai45@gmail.com>
2021-03-02 00:43:59 -0600

    ability scriptable obj. add essential player to all scenes.

---
+++ Assets/Scripts/ScriptableObjects/PlayerAbility.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Ability", menuName = "Player/PlayerAbility")]
+public class PlayerAbility : ScriptableObject
+{
+    public bool shield;
+    public bool grapple;
+}
