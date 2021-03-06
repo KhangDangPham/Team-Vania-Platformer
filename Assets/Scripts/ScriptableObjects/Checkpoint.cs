ba983b410f4638e334a4c662d08a78ddcfa10f1a
trithai1 <trithai45@gmail.com>
2021-03-06 17:42:44 -0600

    adding checkpoint and fixing merge issues

---
+++ Assets/Scripts/ScriptableObjects/Checkpoint.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO Checkpoint", menuName = "SO/Checkpoint")]
+public class Checkpoint : ScriptableObject
+{
+}
